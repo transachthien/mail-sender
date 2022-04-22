6031b3bb80da9d50e909b15a9309dc1ba541fdd6
transachthien <thientshe141579@fpt.edu.vn>
2022-04-22 16:27:56 +0700

    Initial commit

---
+++ src/main/java/com/example/mailsender/controller/MyConstants.java
@@ -0,0 +1,14 @@
+package com.example.mailsender.controller;
+
+public class MyConstants {
+
+    // Replace with your email here:
+    public static final String MY_EMAIL = "transachthien@gmail.com";
+
+    // Replace password!!
+    public static final String MY_PASSWORD = "thien123";
+
+    // And receiver!
+    public static final String FRIEND_EMAIL = "thientshe141579@fpt.edu.vn";
+
+}
